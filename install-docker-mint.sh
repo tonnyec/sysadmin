a89072ee1c57517c3f5186b6f08fed3ef6eda3ab
Luis Antonio Burbano <tonnydeveloper@gmail.com>
2022-05-17 23:47:21 -0500

    Create install-docker-mint.sh

---
+++ install-docker-mint.sh
@@ -0,0 +1,6 @@
+# Install docker ce in linux mint 20.3 una
+
+apt install apt-transport-https ca-certificates curl gnupg lsb-release
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu focal stable" > /etc/apt/sources.list.d/docker.list
+apt install docker-ce docker-ce-cli containerd.io
